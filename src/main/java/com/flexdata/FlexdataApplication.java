5d9028b929843d606ea3ae797338ca0baaafff54
AdmiralMeowington <dest.oblivion@gmail.com>
2024-10-22 16:44:36 +0300

    build
    Spring: Lombok, Spring Web

---
+++ src/main/java/com/flexdata/FlexdataApplication.java
@@ -0,0 +1,13 @@
+package com.flexdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexdata/FlexdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
